bef4ca90fb7396ea0e4af57461aacb067b57c836
John Fernandez <fernandezjohn@icloud.com>
2024-05-13 22:11:04 -0400

    Adding matlab A matrix and b vector in for comparison, fixed coupling integrals

--- src/coupling.py
+++ src/coupling.py
@@ -80,33 +80,63 @@ def A_nj2(n, j):
     else:
         raise ValueError("Invalid values for n and j")
 
-def nk_sigma_helper(mk): 
-    top = sin(2 * h * mk)
-    bottom = 4 * h * mk
-    sigma1 = sqrt(top/bottom + 1/2)
-    sigma2 = sinh(m0 * (d2 - h)) 
-    sigma3 = mk * (d2 - h)
-    sigma4 = sq(pi) * sq(n)
-    sigma5 = sinh(2 * h * m0)
-    return sigma1, sigma2, sigma3, sigma4, sigma5
+def nk_sigma_helper(mk, k, m):
+    sigma1 = sqrt(sinh(2 * h * m0) + 2 * h * m0) / h
+    sigma2 = sin(mk * (d2 - h))
+    sigma3 = pi ** 2 * m ** 2
+    sigma4 = sinh(m0 * (d2 - h))
+    sigma6 = 2 * h * mk
+    sigma5 = sqrt(sin(sigma6) / sigma6 + 1)
+    
+    return sigma1, sigma2, sigma3, sigma4, sigma5, sigma6
 
-def A_km(k, n):
+def A_mk(m, k):
     mk = m_k(k)
-    sigma1, sigma2, sigma3, sigma4, sigma5 = nk_sigma_helper(mk)
-    if k == 0 and n == 0:
-        return (-2 * sqrt(h) * sigma2) / (sqrt(m0) * sqrt(sigma5 + 2 * h * m0))
-    elif 1 <= k and n == 0:
-        return -sin(sigma3) / (mk * sigma1)
-    elif k == 0 and 1 <= n:
-        top = -sqrt(2) * (m0 * (d2 * cos(pi * n) * sigma2 - h * cos(pi * n) * sigma2) * (d2 - h) + pi * n * sin(pi * n) * cosh(m0 * (d2 - h)) * (d2 - h))
-        bottom = sqrt((sigma5 / (4 * h * m0)) + 1/2) * (sq(d2) * sq(m0) - 2 * d2 * h * sq(m0) + sq(h) * sq(m0) + sigma4)
-        return top / bottom
-    elif 1 <= k and 1 <= n:
-        top = -sqrt(2) * (mk * (d2 * sin(sigma3) * cos(pi * n) - h * sin(sigma3) * cos(pi * n)) * (d2 - h) - pi * n * cos(sigma3) * sin(pi * n) * (d2 - h))
-        bottom = sigma1 * (sq(d2) * sq(mk) - 2 * d2 * h * sq(mk) + sq(h) * sq(mk) - sigma4)
-        return top / bottom
-    else:
-        raise ValueError("Invalid values for n and k")
+    sigma1, sigma2, sigma3, sigma4, sigma5, sigma6 = nk_sigma_helper(mk, k, m)
+
+    if k == 0 and m == 0:
+        C_mk = -2 * sigma4 / (sqrt(m0) * sigma1)
+    elif 1 <= k and m == 0:
+        C_mk = -sqrt(2) * sigma2 / (mk * sigma5)
+    elif k == 0 and 1 <= m:
+        C_mk = (2 * (-1)**m * sqrt(2) * m0**(3/2) * sigma4 * (d2 - h)**2) / \
+               (sigma1 * (d2**2 * m0**2 - 2 * d2 * h * m0**2 + h**2 * m0**2 + sigma3))
+    elif 1 <= k and 1 <= m:
+        C_mk = (2 * (-1)**m * sigma2 * mk * (d2 - h)**2) / \
+               (sigma5 * (d2**2 * mk**2 - 2 * d2 * h * mk**2 + h**2 * mk**2 - sigma3))
+    else: 
+        raise ValueError("Invalid values for m and k")
+    
+    return C_mk
+
+
+# def nk_sigma_helper(mk): 
+#     top = sin(2 * h * mk)
+#     bottom = 4 * h * mk
+#     sigma1 = sqrt(top/bottom + 1/2)
+#     sigma2 = sinh(m0 * (d2 - h)) 
+#     sigma3 = mk * (d2 - h)
+#     sigma4 = sq(pi) * sq(n)
+#     sigma5 = sinh(2 * h * m0)
+#     return sigma1, sigma2, sigma3, sigma4, sigma5
+
+# def A_km(k, n):
+#     mk = m_k(k)
+#     sigma1, sigma2, sigma3, sigma4, sigma5 = nk_sigma_helper(mk)
+#     if k == 0 and n == 0:
+#         return (-2 * sqrt(h) * sigma2) / (sqrt(m0) * sqrt(sigma5 + 2 * h * m0))
+#     elif 1 <= k and n == 0:
+#         return -sin(sigma3) / (mk * sigma1)
+#     elif k == 0 and 1 <= n:
+#         top = -sqrt(2) * (m0 * (d2 * cos(pi * n) * sigma2 - h * cos(pi * n) * sigma2) * (d2 - h) + pi * n * sin(pi * n) * cosh(m0 * (d2 - h)) * (d2 - h))
+#         bottom = sqrt((sigma5 / (4 * h * m0)) + 1/2) * (sq(d2) * sq(m0) - 2 * d2 * h * sq(m0) + sq(h) * sq(m0) + sigma4)
+#         return top / bottom
+#     elif 1 <= k and 1 <= n:
+#         top = -sqrt(2) * (mk * (d2 * sin(sigma3) * cos(pi * n) - h * sin(sigma3) * cos(pi * n)) * (d2 - h) - pi * n * cos(sigma3) * sin(pi * n) * (d2 - h))
+#         bottom = sigma1 * (sq(d2) * sq(mk) - 2 * d2 * h * sq(mk) + sq(h) * sq(mk) - sigma4)
+#         return top / bottom
+#     else:
+#         raise ValueError("Invalid values for n and k")
 
 def nk2_sigma_helper(mk): 
     top = sin(2 * h * mk)
