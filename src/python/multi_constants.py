7572e68e3e4e775cd124a3811522dd61cb287782
En Lo <hl752@cornell.edu>
2024-11-18 17:11:51 -0500

    update current progress on slant, hydro coeffs are too small. Get back later

--- src/python/multi_constants.py
+++ src/python/multi_constants.py
@@ -9,3 +9,17 @@ g = 9.81
 n = 3
 z = 6
 # omega = 2 -> calculate omega from m0, g
+
+
+####for RM3 slant study ###
+# import numpy as np
+# h = 50.0
+# d = np.array([29.0, 7.0, 5.5, 4.0])
+# a = np.array([3.0, 5.0, 7.5, 10.0])
+# heaving = [0, 1, 1, 1] # 0/false if not heaving, 1/true if yes heaving
+# slant = [0, 1, 1, 1] # 0/false if not slanted, 1/true if yes slanted
+# n = 3
+# z = 6
+# omega = 0.4
+# # omega = np.linspace(0.1, 1.5, 15)
+# m0 = omega**2/9.81
