6a37a8f1d73d5ae24e1cb015b20a3c4c4df0c29c
Hope Best <hmb95@cornell.edu>
2026-05-28 15:43:34 -0400

    test: implement strict abstract properties on mock class to fix validation pathway type alignment

--- package/test/test_geometry.py
+++ package/test/test_geometry.py
@@ -293,11 +293,14 @@ def test_heaving_count_validation_pathways():
         ConcentricBodyGroup(bodies)
 
     # 2. Test the BodyArrangement base constructor pathway explicitly
-    # We create a minimal mock class that satisfies the ABC contract completely
     class CustomGeometryArrangement(BodyArrangement):
+        @property
         def a(self) -> np.ndarray: return np.array([])
+        @property
         def d(self) -> np.ndarray: return np.array([])
+        @property
         def slant_angle(self) -> np.ndarray: return np.array([])
+        @property
         def heaving(self) -> np.ndarray: return np.array([])
 
     with pytest.raises(ValueError, match="Only 0 or 1 body can be marked as heaving"):
