d46cb9086c2405d1eb0b735626a3701f1a4e2aa3
Rebecca McCabe <chessgirl2400@gmail.com>
2026-06-12 16:51:16 -0400

    repair sea-lab-utils submodule that was accidentally committed as regular files

--- analysis/sea-lab-utils/plotutilities/add_arrow.m
+++
@@ -1,4 +0,0 @@
-% Insert an Arrow with a circle head
-hdumdum = annotation('arrow');
-set(hdumdum,'HeadStyle','ellipse')
-set(hdumdum,'HeadSize',4)
--- analysis/sea-lab-utils/pyplotutilities/fonts.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
- 
-def get_fonts():
-    plt.rcParams.update({'font.family': 'serif', 'text.usetex': True})
-
-#Note: must have MiKTeX or TeX installed on computer
