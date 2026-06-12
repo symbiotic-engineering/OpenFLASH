4ec1c323d2f6a04f452ff81de8f1c6ac258a403e
Rebecca McCabe <chessgirl2400@gmail.com>
2026-06-12 16:51:16 -0400

    repair sea-lab-utils submodule that was accidentally committed as regular files

--- analysis/sea-lab-utils/README.md
+++
@@ -1,2 +0,0 @@
-# sea-lab-utils
-Utilities for plotting and other routine tasks used across the lab
--- analysis/sea-lab-utils/plotutilities/MatlabFontSize_ClassDemos.m
+++
@@ -1,5 +0,0 @@
-s = settings
-s.matlab.fonts.codefont.Size.PersonalValue = 20; %set the font size
-return
-%% To clear the personal value for the font size....
-clearPersonalValue(s.matlab.fonts.codefont.Size)
